09f934186ec559f0de7b27101c3772022fe90ccd
soloplxya <69782911+soloplxya@users.noreply.github.com>
2022-01-18 14:17:05 +0800

    fix: updated typo in origins link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
     origins 'http://localhost:3000'
-    origins 'https://many-todos-backend.herokuapp.com'
+    origins 'https://many-todos.herokuapp.com'
   
     resource '*',
       headers: :any,
