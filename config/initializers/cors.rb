16329483c47f035fd2b38db606f4550fd02874dc
soloplxya <69782911+soloplxya@users.noreply.github.com>
2022-01-18 14:13:09 +0800

    updated cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-    origins '*'
+    origins 'http://localhost:3000'
+    origins 'https://many-todos-backend.herokuapp.com'
   
     resource '*',
       headers: :any,
